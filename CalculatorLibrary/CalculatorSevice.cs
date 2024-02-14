37bafddec7ba15ee02981c5ba64c6f3954eee790
Dademu <a00278997@mycambrian.ca>
2024-02-13 20:49:00 -0500

    Method of addition, subtract e.c created in class CalculatorService

--- CalculatorLibrary/CalculatorSevice.cs
+++ CalculatorLibrary/CalculatorSevice.cs
@@ -2,5 +2,37 @@
 
 public class CalculatorSevice
 {
+    public decimal Add(decimal num1, decimal num2)
+    {
+        return num1 + num2;
+    }
 
+    public decimal Subtract(decimal num1, decimal num2)
+    {
+        return num1 - num2;
+    }
+
+    public decimal Multiply(decimal num1, decimal num2)
+    {
+        return num1 * num2;
+    }
+
+    public decimal Divide(decimal num1, decimal num2)
+    {
+        if (num2 == 0)
+        {
+            throw new ArgumentException("Cannot divide by zero.");
+        }
+        return num1 / num2;
+    }
+
+    public decimal Modulo(decimal num1, decimal num2)
+    {
+        if (num2 == 0)
+        {
+            throw new ArgumentException("Cannot calculate modulo with zero divisor.");
+        }
+        return num1 % num2;
+    }
 }
+
